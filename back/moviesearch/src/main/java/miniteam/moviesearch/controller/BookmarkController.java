716ac3681b1abb2c958cb60e6928824f39239c72
HLuKing <cho04k@dongguk.ac.kr>
2025-04-30 18:49:20 +0900

    북마크 api 기능 구현 완료 (프론트엔드 js에서 이미 북마크에 있는 영화인지 구별해서 작동하게 해야함)

--- back/moviesearch/src/main/java/miniteam/moviesearch/controller/BookmarkController.java
+++ back/moviesearch/src/main/java/miniteam/moviesearch/controller/BookmarkController.java
@@ -11,23 +11,35 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController // 북마크 관련 REST API
-@RequestMapping // /api/bookmarks 로 시작하는 모든 요청 처리
+@RequestMapping("/api/bookmarks") // /api/bookmarks 로 시작하는 모든 요청 처리
 @RequiredArgsConstructor
 public class BookmarkController {
 
     private final BookmarkService bookmarkService;
 
     // 북마크 토클(추가 and 삭제)
-    @PostMapping
-    public ResponseEntity<?> toggleBookmark(@RequestBody BookmarkRequest request) {
+    @PostMapping("/likes")
+    public ResponseEntity<?> addBookmark(@RequestBody BookmarkRequest request) {
         //클라이언트가 JSON으로 영화 정보를 보내면 해당 영화에 대한 북마크를 추가 또는 삭제
 
         bookmarkService.toggleBookmark(request.getMovieId(), request.getTitle(), request.getPosterUrl());
-        return ResponseEntity.ok("처리 완료");
+        return ResponseEntity.ok("북마크 추가됨");
+    }
+
+    @DeleteMapping("/unlikes/{movieId}")
+    public ResponseEntity<?> removeBookmark(@PathVariable String  movieId) {
+        bookmarkService.removeBookmark(movieId);
+        return ResponseEntity.ok("북마크 삭제됨");
+    }
+
+    @PostMapping("/toggle")
+    public ResponseEntity<?> toogleBookmark(@RequestBody BookmarkRequest request) {
+        bookmarkService.toggleBookmark(request.getMovieId(), request.getTitle(), request.getPosterUrl());
+        return ResponseEntity.ok("북마크 토글 완료");
     }
 
     // 내가 북마크한 영화들
-    @GetMapping
+    @GetMapping("/lists")
     public ResponseEntity<List<Bookmark>> getBookmarks() {
         return ResponseEntity.ok(bookmarkService.getMyBookmarks());
     } // 현재 로그인한 사용자에 대한 북마크 목록을 조회해서 반환
--- back/moviesearch/src/main/java/miniteam/moviesearch/service/BookmarkService.java
+++ back/moviesearch/src/main/java/miniteam/moviesearch/service/BookmarkService.java
@@ -26,20 +26,34 @@ public class BookmarkService {
     }
 
     // 북마크 토글
-    public void toggleBookmark(String movieId, String title, String posterUrl) {
+    public void addBookmark(String movieId, String title, String posterUrl) {
         User user = getCurrentUser(); // 햔제 사용자 기준으로 북마크 추가/삭제 판단
+
+        boolean exists = bookmarkRepository.findByUserAndMovieId(user, movieId).isPresent();
+        if (!exists){
+            Bookmark b = new Bookmark();
+            b.setUser(user);
+            b.setMovieId(movieId);
+            b.setTitle(title);
+            b.setPosterUrl(posterUrl);
+            bookmarkRepository.save(b);
+        } // 하트 누르면 "토글" 느낌으로 동작하게 됨
+    }
+
+    public void removeBookmark(String movieId) {
+        User user = getCurrentUser();
+
+        bookmarkRepository.findByUserAndMovieId(user, movieId)
+                .ifPresent(bookmarkRepository::delete);
+    }
+
+    public void toggleBookmark(String movieId, String title, String posterUrl) {
+        User user = getCurrentUser();
 
         bookmarkRepository.findByUserAndMovieId(user, movieId)
                 .ifPresentOrElse(
-                        bookmark -> bookmarkRepository.delete(bookmark), //이미 있다면 삭제
-                        () -> { // 없으면 -> 추가
-                            Bookmark b = new Bookmark();
-                            b.setUser(user);
-                            b.setMovieId(movieId);
-                            b.setTitle(title);
-                            b.setPosterUrl(posterUrl);
-                            bookmarkRepository.save(b);
-                        } // 하트 누르면 "토글" 느낌으로 동작하게 됨
+                        bookmarkRepository::delete,
+                        () -> addBookmark(movieId, title, posterUrl)
                 );
     }
 
